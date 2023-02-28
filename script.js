36958e335bcfeb3fe7b4d7526322792ff4f1e2ee
Evan junior <evsioljr@gmail.com>
2023-02-27 23:01:18 -0300

    add imag

---
+++ script.js
@@ -0,0 +1,20 @@
+function skil(){
+    let k = document.getElementById('skills').innerHTML="REACT , NEXT.JS ,VUE.JS "
+    return
+}
+function skil2(){
+    let k = document.getElementById('skills2').innerHTML="JAVASCRIPT, TYPESCRIPT , PYTHON , HTML AND CSS"
+    return
+}
+function skil3(){
+    let k = document.getElementById('skills3').innerHTML="GIT AND GITHUB"
+    return
+}
+function sob(){
+    
+    let s = document.getElementById("sobre").innerHTML="Eu sou um programador front-end junior com habilidades em React, Next.js e Vue.js. Com conhecimentos sólidos em HTML, CSS e JavaScript, tenho experiência em desenvolver aplicações web responsivas e acessíveis. Além disso, sou comprometido e pró-ativo, sempre buscando aprender novas tecnologias para contribuir de forma efetiva em projetos em equipe."
+    let s2 = document.getElementById("sobre2").innerHTML="Estou entusiasmado em me candidatar para esta vaga de trabalho e fazer parte de sua equipe. Com minha experiência em React, Next.js e Vue.js, estou confiante de que posso desempenhar um papel vital no sucesso da empresa. Estou disponível para entrevistas e ansioso para contribuir para a empresa. Obrigado pela oportunidade."
+    
+    return
+
+}
